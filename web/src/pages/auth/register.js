72eea99cefc84dfc2b12cac153ebe05718df6ba7
Devon Mino <devon.minor.00@gmail.com>
2021-03-12 14:43:36 -0500

    Testing env

--- web/src/pages/auth/register.js
+++ web/src/pages/auth/register.js
@@ -2,6 +2,7 @@ import './auth.scss';
 import { Link } from 'react-router-dom';
 
 export const Register = () => {
+    console.log(process.env.SERVER_URL);
     const address = process.env.SERVER_URL || 'http://localhost:4000';
     const formAction = `${address}/api/auth/register`;
     return (
